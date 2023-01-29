b5aa123597588b3cf8b48fe9baa2979a1b2b0f01
Dušan Lazić <lazicdusan1104@gmail.com>
2023-01-29 19:56:27 +0100

    Remove file uploads from 3rd backend

--- backends/korisnici/src/main/java/com/zavod/api/ControllerAdvisor.java
+++ backends/korisnici/src/main/java/com/zavod/api/ControllerAdvisor.java
@@ -1,7 +1,6 @@
 package com.zavod.api;
 
 import com.zavod.exception.EmailAlreadyInUseException;
-import com.zavod.exception.FileUploadException;
 import com.zavod.exception.WrongCredentialsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.io.FileNotFoundException;
 
 @RestControllerAdvice
 public class ControllerAdvisor {
@@ -29,21 +26,5 @@ public class ControllerAdvisor {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .contentType(MediaType.APPLICATION_XML)
                 .body(new ResponseError(HttpStatus.UNAUTHORIZED, "Pogrešna adresa ili lozinka."));
-    }
-
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(FileUploadException.class)
-    public ResponseEntity<ResponseError> handleFileUploadException(FileUploadException e) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .contentType(MediaType.APPLICATION_XML)
-                .body(new ResponseError(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage()));
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(FileNotFoundException.class)
-    public ResponseEntity<ResponseError> handleFileNotFoundException(FileNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .contentType(MediaType.APPLICATION_XML)
-                .body(new ResponseError(HttpStatus.NOT_FOUND, e.getMessage()));
     }
 }
