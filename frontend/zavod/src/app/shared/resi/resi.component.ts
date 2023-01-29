5f202736d7e09ad89219b31a48215a00405aab0a
Pavle <pavleg.19@gmail.com>
2023-01-29 23:26:03 +0100

    Add submitting resenje on frontend

--- frontend/zavod/src/app/shared/resi/resi.component.ts
+++ frontend/zavod/src/app/shared/resi/resi.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/model/user';
 import { AuthService } from 'src/services/auth/auth.service';
+import { ParserService } from 'src/services/parser.service';
+import { HttpRequestService, autorskaBackend, zigBackend } from 'src/services/util/http-request.service';
 
 @Component({
   selector: 'app-resi',
@@ -9,14 +13,20 @@ import { AuthService } from 'src/services/auth/auth.service';
 })
 export class ResiComponent implements OnInit {
 
+
   status : boolean = true;
   loggedUser: User | undefined;
+  obrazlozenje: string = "";
 
   date = new Date()
   dateStr = this.date.toISOString().split('T')[0]
 
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, 
+    private httpService: HttpRequestService, 
+    private router: Router, 
+    private parser: ParserService,
+    private toastr: ToastrService) {
     this.authService.getLoggedUser().subscribe((user: User | undefined) => {
       this.loggedUser = user;
       console.log(this.loggedUser);
@@ -26,6 +36,39 @@ export class ResiComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  podnesi() {
+    let serviceName = this.router.url.split("/")[1];
+    let brojZahteva = this.router.url.split("/")[3];
+    let backend = "";
+
+    if (serviceName == "autorska") {
+      backend = autorskaBackend;
+    } else if (serviceName == "zig") {
+      backend = zigBackend;
+    }
+
+    let resenje = {
+      Resenje: {
+        odluka: {
+          obrazlozenje: this.obrazlozenje,
+          prihvacen: this.status,
+        }
+      }
+    }
+
+    let parsed = this.parser.js2xml(resenje);
+    console.log(parsed);
+    
+    this.httpService.post(backend + "/resenja/" + brojZahteva, parsed).subscribe({
+      next: (data) => {
+        this.toastr.success("Uspešno ste podneli rešenje", "Uspešno");
+      },
+      error: (err) => {
+        this.toastr.error("Došlo je do greške", "Greška");
+      }
+    });
   }
 
 }
